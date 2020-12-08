9f4819a17579150cb2d2810752e32876ee2b99f5
princev89 <vishwakarmaprince833@gmail.com>
2020-12-08 22:36:31 +0530

    Modified QuickSort file

--- Code/QuickSort.cpp
+++ Code/QuickSort.cpp
@@ -1,58 +1,45 @@
-#include <stdio.h>
-#include <conio.h>
-void quick(int a[], int p, int r);
-int part(int a[], int p, int r);
-int a[30], p, r;
-//converting to int main() and add return value at last 
-int main()
-{
-    int n, i;
-    // clrscr();
-    printf("ENTER the size of array ");
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++)
-        scanf("%d", &a[i]);
-    // quick(a, 1, r); quick will take array base address, first -> index and last -> index of array
-    quick(a, 1, n); 
+#include <iostream>
 
-    for (i = 1; i <= n; i++)
-        printf("%d\t", a[i]);
-    // getch(); commenting getch()
+using namespace std;
 
-    return 0;
-    //added return value
-
-}
-void quick(int a[], int p, int r)
-{
-    int q;
-    if (p < r)
-    {
-        q = part(a, p, r);
-        quick(a, p, q - 1);
-        quick(a, q + 1, r);
-    }
-}
-int part(int a[], int p, int r)
-{
-    int x, i, j, temp, temp1;
-    i = p - 1;
-    // x = a[x]; x will have the value of last element 
-    // so the last element in a[] (passed in part function currently) is present at index r
-    // so x will contain a[r] : value at last index 
-    x = a[r];
-    for (j = p; j <= r - 1; j++)
-    {
-        if (a[j] <= x)
-        {
+int partition(int arr[], int l, int r){
+    int temp;
+    int x = arr[r];
+    int i = l - 1;
+    for(int j = l; j <= r - 1 ; j++){
+        if(arr[j] < x){
             i++;
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
-    temp1 = a[i + 1];
-    a[i + 1] = a[r];
-    a[r] = temp1;
+    temp = arr[i+1];
+    arr[i+1] = arr[r];
+    arr[r] = temp;
     return i + 1;
+}
+
+void quickSort(int arr[], int l, int r){
+    if(l < r){
+        int p = partition(arr, l, r);
+        quickSort(arr, l, p - 1);
+        quickSort(arr, p + 1, r);
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of arr\n";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    quickSort(arr, 0, n-1);
+    
+    for(int i = 0; i < n; i++) cout << arr[i] << " ";
+    
+
+    return 0;
 }
