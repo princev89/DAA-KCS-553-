ba6f6f1d0ac1e3f3f68336b8cb2517ce8b8d48f4
princev89 <vishwakarmaprince833@gmail.com>
2020-12-21 22:15:51 +0530

    completed countingsort.cpp

--- Code/countingSort.cpp
+++ Code/countingSort.cpp
@@ -1,48 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// void CountSort(int A[], int B[], int k){
-//     int n = 6;
-//     int  C[n+1];
-//     for(int i = 1; i <= k; i++) C[i] = 0;
 
-//     for(int j = 1; n; j++) C[A[j]] = C[A[j]] + 1;
-
-//     for(int i = 1; i <= k; i++){
-//         C[i] = C[i] + C[i-1];
-//     }
-
-//     for(int j = n; j > 0; j--){
-//         B[C[A[j]]] = A[j];
-//         C[A[j]] = C[A[j]] - 1;
-
-//     }
-
-//     for(int i = 1; i <= n; i++) cout << B[i] << " ";
+int *countSort(vector<int> A,int B[],int mx){
+    int n = A.size();
+    int C[mx];
+    for (int i = 0; i <= mx; i++) C[i] = 0;
+    for(int i = 1; i < n; i++) C[A[i]]++;
+    for (int i = 1; i <= mx; i++) C[i] += C[i-1];
     
-
-// }
+    for(int j = n-1; j > 0; j--){
+        B[C[A[j]]] = A[j];
+        C[A[j]]--;
+    }
+    return B;
+    
+}
 
 int main(){
     int n;
     cin >> n;
-    
-    int A[n+1];
-    int temp, mx = INT_MIN;
-
-    for(int i = 1; i <= n; i++) {
+    vector<int> arr(n+1);
+    int mx = INT_MIN;
+    int temp;
+    for(int i = 1; i <= n; i++){
         cin >> temp;
-        A[i]  = temp;
+        arr[i] = temp;
         mx = max(mx, temp);
     }
-
-    vector<int> B[mx];
-    cout << mx << endl;
-    cout << B.length();
-
-    // CountSort(A, B, mx);
-    // not completed yet
+    cout << "Input Array: ";
+    for(int i = 1; i <= n; i++) cout << arr[i] << " ";
+    int B[n+1];
+    int *result;
+    result = countSort(arr,B, mx);
+    cout << "\nSorted Array: ";
+    for(int i = 1; i <= n; i++) cout << result[i] << " ";
     
-
     return 0;
 }
